cc5195ff7f1d33aa8a449ce04e6b6724643a5bfc
Ankitsinha007 <sinhaankit0710@gmail.com>
2023-10-18 21:20:49 +0530

    Get users details with update

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
-let x = Math.random() * 100;
+// Sample data
+let posts = [];
+let lastActivityTime = null;
 
-let user = prompt("Enter the number to guess right number:- ");
+// Function to simulate creating a post
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+      resolve(posts);
+    }, 1000);
+  });
+}
 
-if(x == user){
-  console.log("Hurrah !! You are right");
+// Function to simulate updating the last user activity time
+function updateLastUserActivityTime() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      lastActivityTime = new Date();
+      resolve(lastActivityTime);
+    }, 1000);
+  });
 }
-else{
-  console.log("Oops ! sorry, Your guess is wrong, Please Do it again");
+
+// Function to simulate deleting the last post
+function deletePost() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (posts.length > 0) {
+        posts.pop();
+        resolve(posts);
+      } else {
+        reject("No posts to delete");
+      }
+    }, 1000);
+  });
 }
+
+// Function to log posts and lastActivityTime
+function logPostsAndActivity() {
+  console.log("Posts:", posts);
+  console.log("Last Activity Time:", lastActivityTime);
+}
+
+// Simulating the scenario
+createPost("First Post")
+  .then((updatedPosts) => {
+    posts = updatedPosts;
+    return updateLastUserActivityTime();
+  })
+  .then((updatedActivityTime) => {
+    lastActivityTime = updatedActivityTime;
+    logPostsAndActivity();
+    return deletePost();
+  })
+  .then((remainingPosts) => {
+    posts = remainingPosts;
+    logPostsAndActivity();
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
